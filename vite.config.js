113e8dc5a839dca739e3c2687bc8b1d64b83967a
Merouan <marwan.elhattaki17@gmail.com>
2026-04-06 20:34:26 +0100

    Revert Vite config to default SPA mode resolving GitHub Action Multi-page path error

--- vite.config.js
+++ vite.config.js
@@ -2,20 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-import { resolve } from 'path'
-
 export default defineConfig({
   base: '/',
   plugins: [
     tailwindcss(),
     react()
   ],
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-        pitch: resolve(__dirname, 'darbox-pitch/index.html'),
-      },
-    },
-  },
 })
