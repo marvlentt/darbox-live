29089e253c874bd5da1db8cbf23922173786e10a
Merouan <marwan.elhattaki17@gmail.com>
2026-04-06 20:27:26 +0100

    Fix React Router Basename for GH Pages and Enable multi-page build for Pitch

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,20 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+import { resolve } from 'path'
+
 export default defineConfig({
   base: '/darbox-live/',
   plugins: [
     tailwindcss(),
     react()
   ],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        pitch: resolve(__dirname, 'darbox-pitch/index.html'),
+      },
+    },
+  },
 })
